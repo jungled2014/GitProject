b0f914ce38e97b18df7b377bc09cfe805d5488d8
sujionghua <jungled2012@gmail.com>
2015-07-07 16:40:17 +0800

    add a TextView text

--- app/src/main/java/com/example/jungled/gitproject/MainActivity.java
+++ app/src/main/java/com/example/jungled/gitproject/MainActivity.java
@@ -4,10 +4,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
